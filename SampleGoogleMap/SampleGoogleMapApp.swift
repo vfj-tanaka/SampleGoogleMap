4136f848878e83dec1f05ffc0df617c5d9ca87fb
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-11-16 10:53:42 +0900

    Initial Commit

---
+++ SampleGoogleMap/SampleGoogleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGoogleMapApp.swift
+//  SampleGoogleMap
+//
+//  Created by mtanaka on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleGoogleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
